5d06c6371418a75691a581f1af5cc8a98f46ae0a
gleatham <leatham.geoffrey@gmail.com>
2024-09-01 10:54:25 -0700

    Fixed all warnings and errors in hashy_FIM.py

--- hashy_FIM/hashy_FIM.py
+++ hashy_FIM/hashy_FIM.py
@@ -9,100 +9,102 @@ with a matching key.
 directory - dictionary
 numDirectories - int 
 '''
-def createHash(directory, numDirectories):
-    newHash = {}
+def create_hash(directory, num_directories):
+    new_hash = {}
     directory = directory
-    numDirectories = numDirectories
-    keys = []
+    num_directories = num_directories
+    # TODO: Read the comment below
+    #keys = [] I'm not sure why this was originally there but I think it was failing without it.
     keys = list(directory.keys())
 
+    # TODO: Change to for loop
     count = 0
-    while(count < numDirectories):
-        tempDir = directory.get(keys[count])
-        SHAhash = hashlib.md5()
+    while count < num_directories:
+        temp_dir = directory.get(keys[count])
+        sha_hash = hashlib.md5()
         try:
-            for root, dirs, files in os.walk(tempDir):
+            for root, dirs, files in os.walk(temp_dir):
                 for names in files:
-                    filepath = os.path.join(root, names)
+                    file_path = os.path.join(root, names)
                     try:
-                        fin = open(filepath, 'rb')
-                    except:
+                        fin = open(file_path, 'rb')
+                    except FileNotFoundError:
                         print("Unable to open file")
-                        fin.close()
                         continue
 
+                    # TODO: Fix the below jank while loop
                     var = True
                     while var:
                         # Read file in chunks
                         buf = fin.read(4096)
                         if not buf:
                             break
-                        SHAhash.update(hashlib.md5(buf).digest())
+                        sha_hash.update(hashlib.md5(buf).digest())
 
                     fin.close()
 
-        except:
+        except NotADirectoryError:
             print("Hashing failed")
             import traceback
             traceback.print_exc(limit=None, file=None, chain=True)
             return -2
-        newHash.update({keys[count]: SHAhash})
+        new_hash.update({keys[count]: sha_hash})
         count+=1
 
-    return newHash
+    return new_hash
 
 
 '''
 If a new folder is added to the directory list it will create a new hash for it 
 and write it to the file.
 '''
-def writeNewHash(filePath, directories, hashes, numDirectories):
+def write_new_hash(file_path, directories, hashes, num_directories):
     keys = list(hashes.keys())
-    needUpdate = False
+    need_update = False
 
     try:
         for count in range(len(keys)):
-            if(hashes.get(keys[count]) == 'null'):
-                needUpdate = True
-                newKey = createHash(directories, numDirectories)
-                hashes.update({keys[count]: newKey.get(keys[count]).hexdigest()})
+            if hashes.get(keys[count]) == 'null':
+                need_update = True
+                new_key = create_hash(directories, num_directories)
+                hashes.update({keys[count]: new_key.get(keys[count]).hexdigest()})
             count+=1
 
 
-    except:
+    except IndexError:
         import traceback
         traceback.print_exc(limit=None, file=None, chain=True)
         return -2
 
     try:
-        if(needUpdate == True):
-            with open(filePath) as fin:
-                directoryText = fin.readlines()
+        if need_update:
+            with open(file_path) as fin:
+                directory_text = fin.readlines()
                 fin.close()
 
-            fout = open(filePath, 'w')
+            fout = open(file_path, 'w')
             fout.write("###BEGIN###:::" + '\n')
             fout.close()
-            for line in directoryText:
-                if(line[0] == '#'):
-                    if(line.strip() == '###BEGIN###:::'):
+            for line in directory_text:
+                if line[0] == '#':
+                    if line.strip() == '###BEGIN###:::':
                         continue
                     else:
-                        fout = open(filePath, 'a')
+                        fout = open(file_path, 'a')
                         fout.write(line)
                         fout.close()
                 else:
                     #This is where the file updating happens
-                    tempLine = line.split(':', 3)
-                    key = tempLine[0]
-                    path = directories.get(tempLine[0])
-                    shisha = hashes.get(tempLine[0])
-                    fout = open(filePath, 'a')
+                    temp_line = line.split(':', 3)
+                    key = temp_line[0]
+                    path = directories.get(temp_line[0])
+                    shisha = hashes.get(temp_line[0])
+                    fout = open(file_path, 'a')
                     fout.write(str(key) + ":" + str(path) + ":" + str(shisha) + ":" + '\n')
                     fout.close()
 
 
-    except:
+    except FileNotFoundError:
         import traceback
         traceback.print_exc(limit=None, file=None, chain=True)
         return -2
@@ -113,20 +115,20 @@ def writeNewHash(filePath, directories, hashes, numDirectories):
 Reads in a directory path from a file.
 Directory path is used in createHash()
 '''
-def getDirectory(filePath):
+def get_directory(file_path):
     directories = {}
 
     try:
-        with open(filePath) as fin:
-            directoriesList = fin.readlines()
+        with open(file_path) as fin:
+            directories_list = fin.readlines()
 
-            for counter in range(len(directoriesList)):
-                tempDirList = directoriesList[counter].split(':', 3)
-                if (tempDirList[0][0] != "#"):
-                    directories.update({tempDirList[0]: tempDirList[1]})
+            for counter in range(len(directories_list)):
+                temp_dir_list = directories_list[counter].split(':', 3)
+                if temp_dir_list[0][0] != "#":
+                    directories.update({temp_dir_list[0]: temp_dir_list[1]})
                 counter += 1
 
-    except:
+    except NotADirectoryError:
         print("getDirectory() failed")
         import traceback
         traceback.print_exc(limit=None, file=None, chain=True)
@@ -138,18 +140,18 @@ def getDirectory(filePath):
 '''
 Reads file to get keys and hashes
 '''
-def getHashes(filePath):
+def get_hashes(file_path):
     hashes = {}
     try:
-        with open(filePath) as fin:
-            hashesList = fin.readlines()
+        with open(file_path) as fin:
+            hashes_list = fin.readlines()
 
-            for counter in range(len(hashesList)):
-                tempHashList = hashesList[counter].split(':', 3)
-                if(tempHashList[0][0] != "#"):
-                    hashes.update({tempHashList[0]: tempHashList[2]})
+            for counter in range(len(hashes_list)):
+                temp_hash_list = hashes_list[counter].split(':', 3)
+                if temp_hash_list[0][0] != "#":
+                    hashes.update({temp_hash_list[0]: temp_hash_list[2]})
                 counter+=1
-    except:
+    except FileNotFoundError:
         print("getHashes() Failed")
         import traceback
         traceback.print_exc(limit=None, file=None, chain=True)
@@ -162,51 +164,51 @@ Receives two dictionaries and compares hashes based on matching keys
 Returns dictionary with the same key as all the other dicts
 If the hashes match the value is True else False
 '''
-def compareHash(hashes, newHash, numDirectories):
+def compare_hash(hashes, new_hash, num_directories):
     keys = list(hashes.keys())
 
-    compareResults = {}
+    compare_results = {}
 
-    for count in range(numDirectories):
-        hashOne = hashes.get(keys[count])
-        hashTwo = newHash.get(keys[count]).hexdigest()
+    for count in range(num_directories):
+        hash_one = hashes.get(keys[count])
+        hash_two = new_hash.get(keys[count]).hexdigest()
 
-        if(hashOne == str(hashTwo)):
-            compareResults.update({keys[count]: True})
+        if hash_one == str(hash_two):
+            compare_results.update({keys[count]: True})
         else:
-            compareResults.update({keys[count]: False})
+            compare_results.update({keys[count]: False})
 
         count+=1
 
-    return compareResults
+    return compare_results
 '''
 Writes data to log based on whether hashes match or not
 '''
-def writeLog(compareResults, directories):
+def write_log(compare_results, directories):
     date = datetime.today()
     date = date.strftime('%Y-%m-%d')
 
     file = 'C:\\Users\\Geoff\\Documents\\hashTest\\' + 'log' + date + '.txt'
 
     fout = open(file, 'w')
-    logHeader = "Directory hash report for: " + date + '\n'
-    fout.write(logHeader)
+    log_header = "Directory hash report for: " + date + '\n'
+    fout.write(log_header)
     fout.close()
 
     try:
         fout = open(file, 'a')
 
 
-        for name, value in compareResults.items():
-            if(value == True):
+        for name, value in compare_results.items():
+            if value:
                 value = "PASS"
             else:
                 value = "**FAILED HASH**"
             data = (name + "  :" + directories.get(str(name)) + "..." + str(value) + '\n')
             fout.write(data)
 
-    except:
-        print("writeLog() Failed")
+    except FileNotFoundError:
+        print("write_log() Failed")
         import traceback
         traceback.print_exc(limit=None, file=None, chain=True)
         return -2
@@ -216,18 +218,18 @@ def writeLog(compareResults, directories):
 
 def main():
     #add function at top to get filepath
-    filePath = 'C:\\Users\\Geoff\\Documents\\hashTest\\directories.txt'
+    file_path = 'C:\\Users\\Geoff\\Documents\\hashTest\\directories.txt'
 
     #Dictionaries for directory path and hash
-    directories = getDirectory(filePath)
-    hashes = getHashes(filePath)
+    directories = get_directory(file_path)
+    hashes = get_hashes(file_path)
 
-    numDirectories = len(directories)
-    writeNewHash(filePath, directories, hashes, numDirectories)
-    newHash = createHash(directories, numDirectories)
+    num_directories = len(directories)
+    write_new_hash(file_path, directories, hashes, num_directories)
+    new_hash = create_hash(directories, num_directories)
 
-    compareResults = compareHash(hashes, newHash, numDirectories)
-    writeLog(compareResults, directories)
+    compare_results = compare_hash(hashes, new_hash, num_directories)
+    write_log(compare_results, directories)
 
 if __name__ == "__main__":
     main()
--- main.sh
+++ main.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 check_disk_space = "Check Disk Space"
+hasy_FIM = "Hashy FIM"
+mount_status = "Mount Status"
+
+check_disk_space_path = "./check_disk_space/main.py"
+hashy_FIM_path = "./hashy_FIM/hashy_FIM.py"
