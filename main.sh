788673c4986ae14ddaaa534d11c3cad30e4219ca
gleatham <leatham.geoffrey@gmail.com>
2024-12-07 17:47:26 -0800

    qdialog in main working.

--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-check_disk_space = "Check Disk Space"
-hasy_FIM = "Hashy FIM"
-mount_status = "Mount Status"
+check_disk_space="Check Disk Space"
+hasy_FIM="Hashy FIM"
+mount_status="Mount Status"
 
-check_disk_space_path = "./check_disk_space/main.py"
-hashy_FIM_path = "./hashy_FIM/hashy_FIM.py"
-mount_status = "./mount_status/mount_status.py"
+check_disk_space_path="./check_disk_space/main.py"
+hashy_FIM_path="./hashy_FIM/hashy_FIM.py"
+mount_status="./mount_status/mount_status.py"
 
 # Display a menu to choose the script
 choice=$(dialog --clear --backtitle "Geoff's Admin Tools" \
